47d166d89f038d6beb6e99846a6d24abe847657a
YeongSu1000 <rnjsdlsals0@naver.com>
2025-07-28 19:22:07 +0900

    TodoListController 구현

--- Todo-app/src/main/java/org/zerock/todoapp/controller/TodoListController.java
+++ Todo-app/src/main/java/org/zerock/todoapp/controller/TodoListController.java
@@ -1,4 +1,18 @@
 package org.zerock.todoapp.controller;
 
-public class TodoListController {
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "todoListController" , urlPatterns = "/todo/list")
+public class TodoListController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/todo/list.jsp");
+        dispatcher.forward(req, resp);
+    }
 }
