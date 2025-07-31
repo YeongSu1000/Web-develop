721406598d89991c6e9f009f004866618c752a57
YeongSu1000 <rnjsdlsals0@naver.com>
2025-07-31 15:58:28 +0900

    서비스 객체를 가져와서 JSP로 전달

--- Todo-app/src/main/java/org/zerock/todoapp/controller/TodoListController.java
+++ Todo-app/src/main/java/org/zerock/todoapp/controller/TodoListController.java
@@ -1,5 +1,8 @@
 package org.zerock.todoapp.controller;
 
+import org.zerock.todoapp.dto.TodoDTO;
+import org.zerock.todoapp.service.TodoService;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -7,11 +10,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "todoListController" , urlPatterns = "/todo/list")
 public class TodoListController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+
+        System.out.println("/todo/list");
+        List<TodoDTO> dtoList = TodoService.INSTANCE.getList();
+        req.setAttribute("list", dtoList);
+
         RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/todo/list.jsp");
         dispatcher.forward(req, resp);
     }
