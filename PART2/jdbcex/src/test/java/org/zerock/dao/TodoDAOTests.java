3ba7bf27b5daa49769b46f9efcfb166079a0ef50
YeongSu1000 <rnjsdlsals0@naver.com>
2025-08-14 19:28:07 +0900

    Lombok의 @Cleanup 이용하는 방법

--- PART2/jdbcex/src/test/java/org/zerock/dao/TodoDAOTests.java
+++ PART2/jdbcex/src/test/java/org/zerock/dao/TodoDAOTests.java
@@ -1,8 +1,14 @@
 package org.zerock.dao;
 
+import lombok.Cleanup;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.zerock.jdbcex.dao.ConnectionUtil;
 import org.zerock.jdbcex.dao.TodoDAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class TodoDAOTests {
 
@@ -17,4 +23,18 @@ public class TodoDAOTests {
     public void testTime() throws Exception {
         System.out.println(todoDAO.getTime());
     }
+
+    public String getTime2() throws Exception {
+
+        @Cleanup Connection connection = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup PreparedStatement preparedStatement = connection.prepareStatement("select now()");
+        @Cleanup ResultSet resultSet = preparedStatement.executeQuery();
+
+        resultSet.next();
+
+        String now = resultSet.getString(1);
+
+        return now;
+    }
+
 }
