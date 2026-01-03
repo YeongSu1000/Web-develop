38d4b124fdc582c6a8aee63138671c885d0dba9b
YeongSu1000 <rnjsdlsals0@naver.com>
2026-01-03 17:34:29 +0900

    테스트 환경에서 @Log4j2 사용

--- PART2/jdbcex/src/test/java/org/zerock/service/TodoServiceTests.java
+++ PART2/jdbcex/src/test/java/org/zerock/service/TodoServiceTests.java
@@ -1,5 +1,6 @@
 package org.zerock.service;
 
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.zerock.jdbcex.dto.TodoDTO;
@@ -7,6 +8,8 @@ import org.zerock.jdbcex.service.TodoService;
 
 import java.time.LocalDate;
 
+
+@Log4j2
 public class TodoServiceTests {
     private TodoService todoService;
 
@@ -21,6 +24,10 @@ public class TodoServiceTests {
                 .title("JDBC Test Title")
                 .dueDate(LocalDate.now())
                 .build();
+
+        log.info("---------------------------------");
+        log.info(todoDTO);
+
         todoService.register(todoDTO);
     }
 }
