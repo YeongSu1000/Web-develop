6ff7e9f470d1d6ad69317608370002e591edabdf
YeongSu1000 <rnjsdlsals0@naver.com>
2026-01-06 20:22:27 +0900

    PART3 프로젝트 생성 및 PART2 소스 이식

---
+++ PART3/w2/src/main/java/org/zerock/w2/dao/TodoDAO.java
@@ -0,0 +1,128 @@
+package org.zerock.w2.dao;
+
+import lombok.Cleanup;
+import org.zerock.w2.domain.TodoVO;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoDAO {
+
+    // ConnectionUtil 사용하는 코드
+    public String getTime() {
+
+        String now = null;
+
+        try (Connection connection = ConnectionUtil.INSTANCE.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("select now()");
+             ResultSet resultSet = preparedStatement.executeQuery();
+        ) {
+            resultSet.next();
+            now = resultSet.getString(1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return now;
+    }
+
+    // try catch 대신 Lombok 의 @Cleanup 이용하는 방법
+    public String getTime2() throws Exception {
+
+        @Cleanup Connection connection = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup PreparedStatement preparedStatement = connection.prepareStatement("select now()");
+        @Cleanup ResultSet resultSet = preparedStatement.executeQuery();
+
+        resultSet.next();
+
+        String now = resultSet.getString(1);
+
+        return now;
+    }
+
+    // TodoVO 객체를 데이터베이스에 추가하는 기능 (등록)
+    public void insert(TodoVO vo) throws Exception {
+        String sql = "insert into tbl_todo (title, dueDate, finished) values (?, ?, ?)";
+
+        @Cleanup Connection connection = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+        preparedStatement.setString(1, vo.getTitle());
+        preparedStatement.setDate(2, Date.valueOf(vo.getDueDate()));
+        preparedStatement.setBoolean(3, vo.isFinished());
+
+        preparedStatement.executeUpdate();
+    }
+
+    // tbl_todo 내의 모든 데이터를 가져오는 기능 (목록)
+    public List<TodoVO> selectAll() throws Exception {
+
+        String sql = "select * from tbl_todo";
+        @Cleanup Connection connection = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        @Cleanup ResultSet resultSet = preparedStatement.executeQuery();
+
+        List<TodoVO> list = new ArrayList<>();
+
+        while (resultSet.next()) {
+            TodoVO vo = TodoVO.builder().tno(resultSet.getLong("tno"))
+                    .title(resultSet.getString("title"))
+                    .dueDate(resultSet.getDate("duedate").toLocalDate())
+                    .finished(resultSet.getBoolean("finished"))
+                    .build();
+
+            list.add(vo);
+        }
+        return list;
+    }
+
+    // tbl_todo 의 특정 데이터만 가져오는 기능 (조회)
+    public TodoVO selectOne(Long tno) throws Exception {
+        String sql = "select * from tbl_todo where tno =?";
+
+        @Cleanup Connection connection = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+        preparedStatement.setLong(1, tno);
+
+        @Cleanup ResultSet resultSet = preparedStatement.executeQuery();
+
+        resultSet.next();
+        TodoVO vo = TodoVO.builder()
+                .tno(resultSet.getLong("tno"))
+                .title(resultSet.getString("title"))
+                .dueDate(resultSet.getDate("dueDate").toLocalDate())
+                .finished(resultSet.getBoolean("finished"))
+                .build();
+        return vo;
+    }
+
+    // 삭제
+    public void deleteOne(Long tno) throws Exception {
+        String sql = "delete from tbl_todo where tno = ?";
+
+        @Cleanup Connection connection = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+        preparedStatement.setLong(1, tno);
+        preparedStatement.executeUpdate();
+    }
+
+    // 수정
+    public void updateOne(TodoVO todoVo) throws Exception {
+        String sql = "update tbl_todo set title =?, dueDate = ?, finished = ? where tno =?";
+
+        @Cleanup Connection connection = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+        preparedStatement.setString(1, todoVo.getTitle());
+        preparedStatement.setDate(2, Date.valueOf(todoVo.getDueDate()));
+        preparedStatement.setBoolean(3, todoVo.isFinished());
+        preparedStatement.setLong(4, todoVo.getTno());
+
+        preparedStatement.executeUpdate();
+    }
+}
