ffb3f3c0c3a439d6ca748fac0f0f7d40a7bb7957
YeongSu1000 <rnjsdlsals0@naver.com>
2026-01-20 14:52:36 +0900

    POST방식 로그인 처리 구현

--- PART3/w2/src/main/java/org/zerock/w2/controller/LoginController.java
+++ PART3/w2/src/main/java/org/zerock/w2/controller/LoginController.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/login")
@@ -18,5 +19,23 @@ public class LoginController extends HttpServlet {
         log.info("login get ..........");
 
         req.getRequestDispatcher("/WEB-INF/login.jsp").forward(req, resp);
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        log.info("login post..........");
+
+        String mid = req.getParameter("mid");
+        String mpw = req.getParameter("mpw");
+
+        String str = mid + mpw;
+
+        HttpSession session = req.getSession();
+
+        session.setAttribute("loginInfo", str);
+
+        resp.sendRedirect("/todo/list");
     }
 }
