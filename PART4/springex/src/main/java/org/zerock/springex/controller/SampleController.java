94090c4dbbaf88d7710b21191917b7640c5a501e
YeongSu1000 <rnjsdlsals0@naver.com>
2026-01-29 13:39:15 +0900

    Model파라미터를 이용해 JSP로 데이터 전달

--- PART4/springex/src/main/java/org/zerock/springex/controller/SampleController.java
+++ PART4/springex/src/main/java/org/zerock/springex/controller/SampleController.java
@@ -2,8 +2,11 @@ package org.zerock.springex.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.zerock.springex.dto.TodoDTO;
 
 import java.time.LocalDate;
 
@@ -24,15 +27,26 @@ public class SampleController {
     }
 
     @GetMapping("/ex2")
-    public void ex2(@RequestParam(name = "name", defaultValue = "AAA") String name,@RequestParam(name = "age", defaultValue = "20") int age){
+    public void ex2(@RequestParam(name = "name", defaultValue = "AAA") String name, @RequestParam(name = "age", defaultValue = "20") int age) {
         log.info("ex2..........");
         log.info("name: " + name);
         log.info("age: " + age);
     }
 
     @GetMapping("/ex3")
-    public void ex3(LocalDate dueDate){
+    public void ex3(LocalDate dueDate) {
         log.info("ex3..........");
         log.info("dueDate: " + dueDate);
+    }
+
+    @GetMapping("/ex4")
+    public void ex4(Model model) {
+        log.info("--------------------");
+        model.addAttribute("message", "Hello World");
+    }
+
+    @GetMapping("/ex4_1")
+    public void ex4Extra(@ModelAttribute("dto")TodoDTO todoDTO, Model model){
+        log.info(todoDTO);
     }
 }
