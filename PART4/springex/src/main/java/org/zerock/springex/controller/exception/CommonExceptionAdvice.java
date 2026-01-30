9a09f1c624a23e70663f01f5cca8a1a67b980449
YeongSu1000 <rnjsdlsals0@naver.com>
2026-01-30 15:55:54 +0900

    범용적인 예외 처리

--- PART4/springex/src/main/java/org/zerock/springex/controller/exception/CommonExceptionAdvice.java
+++ PART4/springex/src/main/java/org/zerock/springex/controller/exception/CommonExceptionAdvice.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
+
 @ControllerAdvice
 @Log4j2
 public class CommonExceptionAdvice {
@@ -15,5 +17,22 @@ public class CommonExceptionAdvice {
         log.error(numberFormatException.getMessage());
 
         return "NUMBER FORMAT EXCEPTION";
+    }
+
+    @ResponseBody
+    @ExceptionHandler(Exception.class)
+    public String exceptCommon(Exception exception) {
+        log.error("---------------------------------");
+        log.error(exception.getMessage());
+
+        StringBuffer buffer = new StringBuffer("<ul>");
+        buffer.append("<li>" + exception.getMessage() + "</li>");
+
+        Arrays.stream(exception.getStackTrace()).forEach(stackTraceElement -> {
+            buffer.append("<li>" + stackTraceElement + "</li>");
+        });
+        buffer.append("</ul>");
+
+        return buffer.toString();
     }
 }
