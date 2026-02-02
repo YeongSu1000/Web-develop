64d819cd231dbcf6a24d285737312091ab7a54f8
YeongSu1000 <rnjsdlsals0@naver.com>
2026-02-02 16:01:01 +0900

    Todo 목록 기능 개발

--- PART4/springex/src/main/java/org/zerock/springex/service/TodoServiceImpl.java
+++ PART4/springex/src/main/java/org/zerock/springex/service/TodoServiceImpl.java
@@ -8,6 +8,9 @@ import org.zerock.springex.domain.TodoVO;
 import org.zerock.springex.dto.TodoDTO;
 import org.zerock.springex.mapper.TodoMapper;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @Log4j2
 @RequiredArgsConstructor
@@ -21,5 +24,14 @@ public class TodoServiceImpl implements TodoService {
         TodoVO todoVO = modelMapper.map(todoDTO, TodoVO.class);
         log.info(todoVO);
         todoMapper.insert(todoVO);
+    }
+
+    @Override
+    public List<TodoDTO> getAll() {
+        List<TodoDTO> dtoList = todoMapper.selectAll().stream()
+                .map(vo -> modelMapper.map(vo, TodoDTO.class))
+                .collect(Collectors.toList());
+
+        return dtoList;
     }
 }
