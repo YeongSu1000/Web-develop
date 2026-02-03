65306a77b27a34d47773e78c007451dbfce0d57e
YeongSu1000 <rnjsdlsals0@naver.com>
2026-02-03 21:46:10 +0900

    페이징 처리를 위한 DTO. selectList와 getCount 구현

--- PART4/springex/src/test/java/org/zerock/springex/mapper/TodoMapperTests.java
+++ PART4/springex/src/test/java/org/zerock/springex/mapper/TodoMapperTests.java
@@ -1,5 +1,6 @@
 package org.zerock.springex.mapper;
 
+import com.sun.tools.javac.comp.Todo;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.zerock.springex.domain.TodoVO;
+import org.zerock.springex.dto.PageRequestDTO;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -42,8 +44,20 @@ public class TodoMapperTests {
     }
 
     @Test
-    public void testSelectOne(){
+    public void testSelectOne() {
         TodoVO todoVO = todoMapper.selectOne(3L);
         log.info(todoVO);
+    }
+
+    @Test
+    public void testSelectList() {
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .page(1)
+                .size(10)
+                .build();
+
+        List<TodoVO> voList = todoMapper.selectList(pageRequestDTO);
+
+        voList.forEach(vo -> log.info(vo));
     }
 }
