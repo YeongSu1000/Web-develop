07312a9af0e03d3773d4f688fa01554c02b0dfe7
YeongSu1000 <rnjsdlsals0@naver.com>
2026-02-03 18:37:41 +0900

    Todo 삭제 기능 개발

--- PART4/springex/src/main/java/org/zerock/springex/service/TodoServiceImpl.java
+++ PART4/springex/src/main/java/org/zerock/springex/service/TodoServiceImpl.java
@@ -36,11 +36,16 @@ public class TodoServiceImpl implements TodoService {
     }
 
     @Override
-    public TodoDTO getOne(Long tno){
+    public TodoDTO getOne(Long tno) {
         TodoVO todoVO = todoMapper.selectOne(tno);
 
         TodoDTO todoDTO = modelMapper.map(todoVO, TodoDTO.class);
 
         return todoDTO;
+    }
+
+    @Override
+    public void remove(Long tno) {
+        todoMapper.delete(tno);
     }
 }
