7167047813d8795254444ae97be80f017316b6f2
YeongSu1000 <rnjsdlsals0@naver.com>
2026-02-21 17:17:54 +0900

    Q도메인을 이용한 쿼리 작성 및 테스트

--- PART5/b01/src/main/java/org/zerock/b01/repository/search/BoardSearchImpl.java
+++ PART5/b01/src/main/java/org/zerock/b01/repository/search/BoardSearchImpl.java
@@ -1,9 +1,13 @@
 package org.zerock.b01.repository.search;
 
+import com.querydsl.jpa.JPQLQuery;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.zerock.b01.domain.Board;
+import org.zerock.b01.domain.QBoard;
+
+import java.util.List;
 
 public class BoardSearchImpl extends QuerydslRepositorySupport implements BoardSearch {
 
@@ -13,6 +17,17 @@ public class BoardSearchImpl extends QuerydslRepositorySupport implements BoardS
 
     @Override
     public Page<Board> search1(Pageable pageable) {
+
+        QBoard board = QBoard.board; // Q도메인 객체
+
+        JPQLQuery<Board> query = from(board); // select.. from board
+
+        query.where(board.title.contains("1")); // where title like...
+
+        List<Board> list = query.fetch();
+
+        long count = query.fetchCount();
+
         return null;
     }
 }
