27c10320269a8ab4360292ba26efecc16ba246ac
YeongSu1000 <rnjsdlsals0@naver.com>
2026-02-21 15:08:40 +0900

    Board 엔티티 작성

---
+++ PART5/b01/src/main/java/org/zerock/b01/domain/Board.java
@@ -0,0 +1,18 @@
+package org.zerock.b01.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+    private String title;
+    private String content;
+    private String writer;
+
+}
