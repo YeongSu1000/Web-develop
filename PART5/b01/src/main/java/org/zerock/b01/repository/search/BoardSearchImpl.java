42fc9999a940483980adbdb62666316554ad2ee2
YeongSu1000 <rnjsdlsals0@naver.com>
2026-02-21 17:33:22 +0900

    BooleanBuilder를 이용한 검색 조건 처리

--- PART5/b01/src/main/java/org/zerock/b01/repository/search/BoardSearchImpl.java
+++ PART5/b01/src/main/java/org/zerock/b01/repository/search/BoardSearchImpl.java
@@ -1,5 +1,6 @@
 package org.zerock.b01.repository.search;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.JPQLQuery;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -22,7 +23,12 @@ public class BoardSearchImpl extends QuerydslRepositorySupport implements BoardS
 
         JPQLQuery<Board> query = from(board); // select.. from board
 
-        query.where(board.title.contains("1")); // where title like...
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        booleanBuilder.or(board.title.contains("11"));
+        booleanBuilder.or(board.content.contains("11"));
+
+        query.where(booleanBuilder);
+        query.where(board.bno.gt(0L));
 
         //paging
         this.getQuerydsl().applyPagination(pageable, query);
