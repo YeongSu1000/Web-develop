97d9146cd8ec6451d917be217bd10c0c0ca4f8f1
YeongSu1000 <rnjsdlsals0@naver.com>
2026-02-23 21:17:33 +0900

    목록/검색 처리 : 목록/검색 기능 테스트

--- PART5/b01/src/test/java/org/zerock/b01/service/BoardServiceTests.java
+++ PART5/b01/src/test/java/org/zerock/b01/service/BoardServiceTests.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.b01.dto.BoardDTO;
+import org.zerock.b01.dto.PageRequestDTO;
+import org.zerock.b01.dto.PageResponseDTO;
 
 @SpringBootTest
 @Log4j2
@@ -41,5 +43,21 @@ public class BoardServiceTests {
                 .build();
 
         boardService.modify(boardDTO);
+    }
+
+    @Test
+    public void testList(){
+
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .type("tcw")
+                .keyword("1")
+                .page(1)
+                .size(10)
+                .build();
+
+        PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
+
+        log.info(responseDTO);
+
     }
 }
