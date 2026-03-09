0749a48294242fcea0ecf15db0cc860cd7565c02
YeongSu1000 <rnjsdlsals0@naver.com>
2026-03-09 17:36:14 +0900

    Projection.bean()과 applyPagination() 적용

--- PART6/b01/src/main/java/org/zerock/b01/repository/search/BoardSearchImpl.java
+++ PART6/b01/src/main/java/org/zerock/b01/repository/search/BoardSearchImpl.java
@@ -1,6 +1,7 @@
 package org.zerock.b01.repository.search;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.JPQLQuery;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -92,6 +93,44 @@ public class BoardSearchImpl extends QuerydslRepositorySupport implements BoardS
 
         query.groupBy(board);
 
-        return null;
+        if ((types != null && types.length > 0) && keyword != null) {
+
+            BooleanBuilder booleanBuilder = new BooleanBuilder();
+
+            for (String type : types) {
+
+                switch (type) {
+                    case "t":
+                        booleanBuilder.or(board.title.contains(keyword));
+                        break;
+                    case "c":
+                        booleanBuilder.or(board.content.contains(keyword));
+                        break;
+                    case "w":
+                        booleanBuilder.or(board.writer.contains(keyword));
+                        break;
+                }
+            }
+            query.where(booleanBuilder);
+        }
+
+        query.where(board.bno.gt(0L));
+
+        JPQLQuery<BoardListReplyCountDTO> dtoQuery = query.select(Projections.
+                bean(BoardListReplyCountDTO.class,
+                        board.bno,
+                        board.title,
+                        board.writer,
+                        board.regDate,
+                        reply.count().as("replyCount")
+                ));
+
+        this.getQuerydsl().applyPagination(pageable, dtoQuery);
+
+        List<BoardListReplyCountDTO> dtolist = dtoQuery.fetch();
+
+        long count = dtoQuery.fetchCount();
+
+        return new PageImpl<>(dtolist, pageable, count);
     }
 }
