9d48aae2027188910addd228b8d0f35c8db7d5c5
YeongSu1000 <rnjsdlsals0@naver.com>
2026-03-09 16:14:16 +0900

    @Query를 이용한 페이징 구현

--- PART6/b01/src/test/java/org/zerock/b01/repository/ReplyRepositoryTests.java
+++ PART6/b01/src/test/java/org/zerock/b01/repository/ReplyRepositoryTests.java
@@ -4,9 +4,12 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.zerock.b01.domain.Board;
 import org.zerock.b01.domain.Reply;
-import org.zerock.b01.repository.search.ReplyRepository;
 
 @SpringBootTest
 @Log4j2
@@ -31,5 +34,19 @@ public class ReplyRepositoryTests {
 
         replyRepository.save(reply);
 
+    }
+
+    @Test
+    public void testBoardReplies() {
+
+        Long bno = 100L;
+
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("rno").descending());
+
+        Page<Reply> result = replyRepository.listOfBoard(bno, pageable);
+
+        result.getContent().forEach(reply -> {
+            log.info(reply);
+        });
     }
 }
