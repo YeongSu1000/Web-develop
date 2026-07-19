1d999bf989d123ca3e82333bf21ac66fe38a9f12
YeongSu1000 <rnjsdlsals0@naver.com>
2026-07-19 17:47:42 +0900

    댓글 목록 화면에 출력하는 기능 구현

--- PART6/b01/src/main/resources/static/js/reply.js
+++ PART6/b01/src/main/resources/static/js/reply.js
@@ -5,3 +5,10 @@ async function get1(bno){
 
     return result;
 }
+
+async function getList({bno, page, size, goLast}){
+
+    const result = await axios.get(`/replies/list/${bno}`, {params: {page, size}})
+
+    return result.data
+}
