62252a5fc9d92b9c0183999dfa3d22dc105e3112
YeongSu1000 <rnjsdlsals0@naver.com>
2026-07-19 19:29:01 +0900

    댓글 마지막 페이지로 이동 처리

--- PART6/b01/src/main/resources/static/js/reply.js
+++ PART6/b01/src/main/resources/static/js/reply.js
@@ -1,4 +1,4 @@
-async function get1(bno){
+async function get1(bno) {
     const result = await axios.get(`/replies/list/${bno}`)
 
     // console.log(result)
@@ -6,9 +6,15 @@ async function get1(bno){
     return result;
 }
 
-async function getList({bno, page, size, goLast}){
+async function getList({bno, page, size, goLast}) {
 
     const result = await axios.get(`/replies/list/${bno}`, {params: {page, size}})
+
+    if (goLast) {
+        const total = result.data.total
+        const lastPage = parseInt(Math.ceil(total / size))
+        return getList({bno: bno, page: lastPage, size: size})
+    }
 
     return result.data
 }
