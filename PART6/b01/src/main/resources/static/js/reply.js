9f9da1b012de62a5fb641af061ea8bdca550af79
YeongSu1000 <rnjsdlsals0@naver.com>
2026-07-19 21:54:01 +0900

    댓글 수정 및 삭제 이벤트 처리 완료

--- PART6/b01/src/main/resources/static/js/reply.js
+++ PART6/b01/src/main/resources/static/js/reply.js
@@ -33,3 +33,8 @@ async function modifyReply(replyObj) {
     const response = await axios.put(`/replies/${replyObj.rno}`, replyObj)
     return response.data
 }
+
+async function removeReply(rno) {
+    const response = await axios.delete(`/replies/${rno}`)
+    return response.data
+}
