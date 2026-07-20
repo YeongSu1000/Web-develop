ff2d5ffebf7170800d7d690c013f567ca155e0ac
YeongSu1000 <rnjsdlsals0@naver.com>
2026-07-20 18:41:04 +0900

    첨부파일 저장 및 UUID 이용한 중복 파일 방지

--- PART7/b01/src/main/java/org/zerock/b01/controller/UpDownController.java
+++ PART7/b01/src/main/java/org/zerock/b01/controller/UpDownController.java
@@ -9,6 +9,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.b01.dto.upload.UploadFileDto;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
 @RestController
 @Log4j2
 public class UpDownController {
@@ -19,15 +24,26 @@ public class UpDownController {
     @Operation(summary = "upload Post", description = "POST 방식으로 파일 등록")
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 
-    public String upload(@ModelAttribute UploadFileDto uploadFileDTO){
+    public String upload(@ModelAttribute UploadFileDto uploadFileDTO) {
 
         log.info(uploadFileDTO);
 
-        if(uploadFileDTO.getFiles() != null){
+        if (uploadFileDTO.getFiles() != null) {
 
             uploadFileDTO.getFiles().forEach(multipartFile -> {
 
-                log.info(multipartFile.getOriginalFilename());
+                String originalName = multipartFile.getOriginalFilename();
+                log.info(originalName);
+
+                String uuid = UUID.randomUUID().toString();
+
+                Path savePath = Paths.get(uploadPath, uuid + "_" + originalName);
+
+                try{
+                    multipartFile.transferTo(savePath); // 실제 파일 저장
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
             });
         }
 
