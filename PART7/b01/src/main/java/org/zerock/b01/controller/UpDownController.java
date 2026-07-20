bf0c9f1f008aef977627ad740c86343030561208
YeongSu1000 <rnjsdlsals0@naver.com>
2026-07-20 19:39:42 +0900

    DELETE 방식으로 첨부파일 및 썸네일 삭제

--- PART7/b01/src/main/java/org/zerock/b01/controller/UpDownController.java
+++ PART7/b01/src/main/java/org/zerock/b01/controller/UpDownController.java
@@ -18,9 +18,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 @RestController
 @Log4j2
@@ -95,6 +93,34 @@ public class UpDownController {
             return ResponseEntity.internalServerError().build();
         }
         return ResponseEntity.ok().headers(headers).body(resource);
+    }
+
+    @Operation(summary = "remove 파일", description = "DELETE 방식으로 파일 삭제")
+    @DeleteMapping("/remove/{fileName}")
+    public Map<String, Boolean> removeFile(@PathVariable String fileName) {
+
+        Resource resource = new FileSystemResource(uploadPath + File.separator + fileName);
+        String resourceName = resource.getFilename();
+
+        Map<String, Boolean> resultMap = new HashMap<>();
+        boolean removed = false;
+
+        try {
+            String contextType = Files.probeContentType(resource.getFile().toPath());
+            removed = resource.getFile().delete();
+
+            // 섬네일이 존재한다면
+            if (contextType.startsWith("image")) {
+                File thumbnailFile = new File(uploadPath + File.separator + "s_" + fileName);
+                thumbnailFile.delete();
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+
+        resultMap.put("result", removed);
+
+        return resultMap;
     }
 
 }
