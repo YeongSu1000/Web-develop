f15eed9c3d8c6f5b2e5cdb5acdb45da41ab6d3bb
YeongSu1000 <rnjsdlsals0@naver.com>
2026-07-20 18:53:33 +0900

    Thumbnail 이미지 생성 기능

--- PART7/b01/src/main/java/org/zerock/b01/controller/UpDownController.java
+++ PART7/b01/src/main/java/org/zerock/b01/controller/UpDownController.java
@@ -2,6 +2,7 @@ package org.zerock.b01.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnailator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.b01.dto.upload.UploadFileDto;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.UUID;
@@ -39,9 +42,16 @@ public class UpDownController {
 
                 Path savePath = Paths.get(uploadPath, uuid + "_" + originalName);
 
-                try{
+                try {
                     multipartFile.transferTo(savePath); // 실제 파일 저장
-                }catch (IOException e){
+
+                    // 이미지 파일의 종류라면
+                    if (Files.probeContentType(savePath).startsWith("image")) {
+                        File thumbFile = new File(uploadPath, "s_" + uuid + "_" + originalName);
+
+                        Thumbnailator.createThumbnail(savePath.toFile(), thumbFile, 200, 200);
+                    }
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             });
