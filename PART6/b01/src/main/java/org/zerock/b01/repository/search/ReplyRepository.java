5348caf47b9dac3297172ab5e45fcd266c62bcfa
YeongSu1000 <rnjsdlsals0@naver.com>
2026-07-20 17:45:02 +0900

    PART7 실습을 위한 기존 프로젝트 복사

--- PART6/b01/src/main/java/org/zerock/b01/repository/search/ReplyRepository.java
+++
@@ -1,7 +0,0 @@
-package org.zerock.b01.repository.search;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.zerock.b01.domain.Reply;
-
-public interface ReplyRepository extends JpaRepository<Reply, Long> {
-}
---
+++ PART7/b01/src/main/java/org/zerock/b01/config/SwaggerConfig.java
@@ -0,0 +1,60 @@
+package org.zerock.b01.config;
+
+/* Legacy
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("org.zerock.b01.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Boot 01 Project Swagger")
+                .build();
+    }
+}
+*/
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi api() {
+        return GroupedOpenApi.builder()
+                .group("api") // 기본 그룹명
+                .packagesToScan("org.zerock.b01.controller") // RequestHandlerSelectors.basePackage와 동일
+                .pathsToMatch("/**") // PathSelectors.any()와 동일
+                .build();
+    }
+
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Boot 01 Project Swagger")); // apiInfo() 타이틀과 동일
+    }
+}
