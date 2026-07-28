d8066186d2fa990da4f16437a4aea3ec3239c9be
YeongSu1000 <rnjsdlsals0@naver.com>
2026-07-28 18:29:34 +0900

    로그인 설정 추가

--- PART8/b01/src/main/java/org/zerock/b01/config/CustomSecurityConfig.java
+++ PART8/b01/src/main/java/org/zerock/b01/config/CustomSecurityConfig.java
@@ -19,6 +19,17 @@ public class CustomSecurityConfig {
 
         log.info("---------configure---------");
 
+        // [책의 기존 코드 (Spring Boot 2 / JDK 8 구버전 스타일)]
+        // 책에는 'http.formLogin();'으로 단독 호출하게 되어 있었음.
+        // http.formLogin();
+
+        // [현재 수정 코드 (Spring Boot 3 / JDK 17 람다 스타일)]
+        // Boot 3부터는 람다식을 필수로 요구하므로 아래와 같이 변경함.
+        // 아직 커스텀 로그인 페이지가 없으므로 내부 설정은 비워두어 스프링 기본 로그인창을 쓰도록 유도.
+        http.formLogin(formLogin -> {
+
+        });
+
         return http.build();
     }
 
