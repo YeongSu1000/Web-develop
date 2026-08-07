7429f975c5c43448bb3eba73acb4b9ab3ac0af10
YeongSu1000 <rnjsdlsals0@naver.com>
2026-08-07 18:37:34 +0900

    JSON 기반 API 로그인 처리를 위한 APILoginFilter 구현

--- PART9/api01/src/main/java/org/zerock/api01/security/filter/APILoginFilter.java
+++ PART9/api01/src/main/java/org/zerock/api01/security/filter/APILoginFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
@@ -35,7 +36,11 @@ public class APILoginFilter extends AbstractAuthenticationProcessingFilter {
 
         log.info(jsonData);
 
-        return null;
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                jsonData.get("mid"),
+                jsonData.get("mpw"));
+        return getAuthenticationManager().authenticate(authenticationToken);
+
     }
 
     private Map<String, String> parseRequestJSON(HttpServletRequest request) {
