66166606dc0fa866e7b7c4d99bc09547f47d684a
YeongSu1000 <rnjsdlsals0@naver.com>
2026-08-08 19:18:42 +0900

    JWTUtil 토큰 생성 메서드(generateToken) 구현

--- PART9/api01/src/main/java/org/zerock/api01/util/JWTUtil.java
+++ PART9/api01/src/main/java/org/zerock/api01/util/JWTUtil.java
@@ -1,10 +1,15 @@
 package org.zerock.api01.util;
 
 import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.time.ZonedDateTime;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 @Component
@@ -16,7 +21,28 @@ public class JWTUtil {
 
     public String generateToken(Map<String, Object> valueMap, int days) {
         log.info("generateKey..." + key);
-        return null;
+
+        // 헤더 부분
+        Map<String, Object> header = new HashMap<>();
+        header.put("typ","JWT");
+        header.put("alg","HS256");
+
+        // payload 부분 설정
+        Map<String, Object> payload = new HashMap<>();
+        payload.putAll(valueMap);
+
+        // 테스트 시에는 짧은 유효 기간
+        int time = (1) * days;  // 테스트는 분단위로 나중에 60 * 24 (일) 단위 변경
+
+        String jwtStr = Jwts.builder()
+                .setHeader(header)
+                .setClaims(payload)
+                .setIssuedAt(Date.from(ZonedDateTime.now().toInstant()))
+                .setExpiration(Date.from(ZonedDateTime.now().plusMinutes(time).toInstant()))
+                .signWith(SignatureAlgorithm.HS256, key.getBytes())
+                .compact();
+
+        return jwtStr;
     }
 
     public Map<String, Object> validateToken(String token) throws JwtException {
