178399d9dfa3e6372c2b621e9bc1f4d4b9772ccd
YeongSu1000 <rnjsdlsals0@naver.com>
2026-08-11 16:29:10 +0900

    Swagger UI JWT 인증 설정 추가

--- PART9/api01/src/main/java/org/zerock/api01/config/SwaggerConfig.java
+++ PART9/api01/src/main/java/org/zerock/api01/config/SwaggerConfig.java
@@ -22,6 +22,8 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build()
+                .securitySchemes(List.of(apiKey))) // 추가된 부분
+                .securityContexts(List.of(securityContext())) // 추가된 부분
                 .apiInfo(apiInfo());
     }
 
@@ -30,24 +32,64 @@ public class SwaggerConfig {
                 .title("Boot 01 Project Swagger")
                 .build();
     }
+
+
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Bearer Token", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth())
+                .operationSelector(selector -> selector.requestMappingPattern().startsWith("/api/")).build();
+    }
+
+    private List<SecurityReference> defaultAuth(){
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "global access");
+
+        return List.of(new SecurityReference("Authorization", new AuthorizationScope[] {AuthorizationScope}));
+    }
+
 }
 */
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import org.springdoc.core.models.GroupedOpenApi;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
+    /*
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .components(new Components())
                 .info(apiInfo());
+    }*/
+
+    @Bean
+    public OpenAPI openAPI() {
+        // Security Scheme 정의
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)  // HTTP 타입
+                .scheme("bearer")               // Bearer 토큰
+                .bearerFormat("JWT");           // JWT 형식
+
+        // 보안 요구사항 추가
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("bearerAuth");
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Documentation")
+                        .version("1.0")
+                        .description("API documentation with JWT security"))
+                .addSecurityItem(securityRequirement) // 보안 요구사항 추가
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth", securityScheme));
     }
 
     private Info apiInfo() {
