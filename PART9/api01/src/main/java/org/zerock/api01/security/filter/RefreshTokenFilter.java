ef53dbb624c8caa19c6466725464221797dfe343
YeongSu1000 <rnjsdlsals0@naver.com>
2026-08-12 16:19:14 +0900

    RefreshTokenFilter 요청 JSON 파싱 및 토큰 추출 로직 추가

--- PART9/api01/src/main/java/org/zerock/api01/security/filter/RefreshTokenFilter.java
+++ PART9/api01/src/main/java/org/zerock/api01/security/filter/RefreshTokenFilter.java
@@ -1,5 +1,7 @@
 package org.zerock.api01.security.filter;
 
+import com.google.gson.Gson;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -7,9 +9,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.zerock.api01.security.exception.RefreshTokenException;
 import org.zerock.api01.util.JWTUtil;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Map;
 
 @Log4j2
 @RequiredArgsConstructor
@@ -24,12 +30,36 @@ public class RefreshTokenFilter extends OncePerRequestFilter {
 
         String path = request.getRequestURI();
 
-        if (!path.equals(refreshPath)){
+        if (!path.equals(refreshPath)) {
             log.info("skip refresh token filter........");
             filterChain.doFilter(request, response);
             return;
         }
+
         log.info("Refresh Token Filter...run...............");
+
+        // 전송된 JSON에서 accessToken과 refreshToken을 얻어온다
+        Map<String, String> tokens = parseRequestJSON(request);
+
+        String accessToken = tokens.get("accessToken");
+        String refreshToken = tokens.get("refreshToken");
+
+        log.info("accessToken: " + accessToken);
+        log.info("refreshToken: " + refreshToken);
+    }
+
+    private Map<String, String> parseRequestJSON(HttpServletRequest request){
+
+        try(Reader reader = new InputStreamReader(request.getInputStream())){
+
+            Gson gson = new Gson();
+
+            return gson.fromJson(reader, Map.class);
+
+        }catch(Exception e){
+            log.error(e.getMessage());
+        }
+        return null;
     }
 
 }
