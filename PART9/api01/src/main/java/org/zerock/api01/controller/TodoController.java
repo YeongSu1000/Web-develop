60613ab4f521208d82a5b185df6a59da35918199
YeongSu1000 <rnjsdlsals0@naver.com>
2026-08-15 19:44:29 +0900

    조회 GET API 컨트롤러 구현

--- PART9/api01/src/main/java/org/zerock/api01/controller/TodoController.java
+++ PART9/api01/src/main/java/org/zerock/api01/controller/TodoController.java
@@ -3,10 +3,7 @@ package org.zerock.api01.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.zerock.api01.dto.TodoDTO;
 import org.zerock.api01.service.TodoService;
 
@@ -30,4 +27,10 @@ public class TodoController {
         return Map.of("tno", tno);
     }
 
+    @GetMapping("/{tno}")
+    public TodoDTO read(@PathVariable("tno") Long tno) {
+        log.info("read tno: " + tno);
+
+        return todoService.read(tno);
+    }
 }
